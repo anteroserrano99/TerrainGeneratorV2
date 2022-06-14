ba0a3155143f18c5def416ee2be23c4653475103
anteroserrano99 <serranoantero13@gmail.com>
2022-06-14 20:49:04 +0200

    Initial Commit

---
+++ src/main/kotlin/com/terrain/generator/noisegenerator/NoiseGeneratorApplication.kt
@@ -0,0 +1,11 @@
+package com.terrain.generator.noisegenerator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NoiseGeneratorApplication
+
+fun main(args: Array<String>) {
+	runApplication<NoiseGeneratorApplication>(*args)
+}
---
+++ src/test/kotlin/com/terrain/generator/noisegenerator/NoiseGeneratorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.terrain.generator.noisegenerator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NoiseGeneratorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
